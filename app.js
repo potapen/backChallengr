85eb3b8c9fd0ddef004560fc98dbfe2697c8ab5a
potapen <po@ta.pen>
2022-03-28 15:38:46 +0200

    challenge route in progress

--- app.js
+++ app.js
@@ -31,6 +31,9 @@ app.use("/", index);
 const authRoutes = require("./routes/auth.routes");
 app.use("/auth", authRoutes);
 
+const challengeRoutes = require("./routes/challenge.routes");
+app.use("/challenge", challengeRoutes);
+
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
 
---
+++ routes/challenge.routes.js
@@ -0,0 +1,58 @@
+const router = require("express").Router();
+const Challenge = require("../models/Challenge.model");
+const Game = require("../models/Game.model");
+const League = require("../models/League.model");
+const User = require("../models/User.model");
+
+/* GET home page */
+router.get("/", (req, res, next) => {
+  res.send("challenge!");
+});
+
+router.get("/create", async (req, res, next) => {
+  console.log('req.query: ', req.query)
+  const {league} = req.query
+  if(league){
+    const games = await Game.find(
+      {ownerLeagues: league}
+    );
+    const users = await User.find();
+    const data = {
+      games,
+      users,
+      league,
+    }
+    res.render("challenge/createWithLeagueID", data);
+  }
+  else{
+    const leagues = await League.find();
+    const data = {
+      leagues,
+    }
+    res.render("challenge/create", data);
+  }
+});
+
+router.post("/create", (req, res, next) => {
+  console.log('req.body :', req.body)
+  res.send("create post");
+});
+
+
+module.exports = router;
+
+
+// const challengeSchema = new Schema(
+//     {
+//       contenders: [{ type: Schema.Types.ObjectId, ref: "User", required: true }],
+//       leagues: [{ type: Schema.Types.ObjectId, ref: "League" }],
+//       game: { type: Schema.Types.ObjectId, ref: "Game", required: true },
+//       winners: [{ type: Schema.Types.ObjectId, ref: "User" }],
+//       stake: { type: Number, min: 0, required: true },
+//       isCompleted: { type: Boolean },
+//     },
+//     {
+//       // this second object adds extra properties: `createdAt` and `updatedAt`
+//       timestamps: true,
+//     }
+//   );
