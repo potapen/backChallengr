85577fdf81550884829c3e54599f175ebb509d77
potapen <po@ta.pen>
2022-03-29 12:09:32 +0200

    edit done

--- routes/challenge.routes.js
+++ routes/challenge.routes.js
@@ -3,6 +3,7 @@ const Challenge = require("../models/Challenge.model");
 const Game = require("../models/Game.model");
 const League = require("../models/League.model");
 const User = require("../models/User.model");
+const mongoose = require("mongoose");
 
 /* GET home page */
 
@@ -69,6 +70,23 @@ router.post("/create", async (req, res, next) => {
   res.send("create post")
 });
 
+router.post("/edit/:challengeID", async (req, res, next) => {
+  const {challengeID, contenders, league, game, winners,stake, isCompleted} = req.body
+  const challengeToEdit = {
+    contenders,
+    league,
+    game,
+    winners,
+    stake,
+    isCompleted: Boolean(isCompleted)
+  }
+  // console.log('------------------------challengeToEdit:', challengeToEdit)
+  console.log('------------------------challengeID: ', challengeID);
+  // const challengeUpdated = Challenge.findById(mongoose.Types.ObjectId(challengeID))
+  const challengeUpdated = await Challenge.findByIdAndUpdate(challengeID,challengeToEdit, {new: true});
+  res.send(challengeUpdated);
+});
+
 router.get("/edit/:challengeID", async (req, res, next) => {
   const {challengeID} = req.params
   console.log('---------------------------challengeID: ', challengeID)
@@ -79,27 +97,59 @@ router.get("/edit/:challengeID", async (req, res, next) => {
   .populate('winners')
   .populate('stake')
   .populate('isCompleted')
-  console.log('---------------------------challengeToEdit: ',challengeToEdit)
+  // console.log('---------------------------challengeToEdit: ',challengeToEdit)
   
-  const users = await User.find();
-  users.forEach(person1 => {
+  const usersContender = await User.find(); //pas besoin de .lean()...c'est magigue...
+  usersContender.forEach(person1 => {
     const isSelected = challengeToEdit.contenders.some(person2 => {
         const isSame = JSON.stringify(person1._id) === JSON.stringify(person2._id)
-        console.log(`person1._id: ${JSON.stringify(person1._id)} person2._id: ${JSON.stringify(person2._id)} : ${isSame}`)
+        // console.log(`person1._id: ${JSON.stringify(person1._id)} person2._id: ${JSON.stringify(person2._id)} : ${isSame}`)
+        return isSame
+    })
+    person1.selected = isSelected
+  })
+  
+  const usersWinner = await User.find(); //pas besoin de .lean()...c'est magigue...
+  usersWinner.forEach(person1 => {
+    const isSelected = challengeToEdit.winners.some(person2 => {
+        const isSame = JSON.stringify(person1._id) === JSON.stringify(person2._id)
+        // console.log(`person1._id: ${JSON.stringify(person1._id)} person2._id: ${JSON.stringify(person2._id)} : ${isSame}`)
         return isSame
     })
     person1.selected = isSelected
   })
 
-  const leagues = await League.find();
-  const games = await Game.find();
+  const leagues = await League.find(); //pas besoin de .lean()...c'est magigue...
+
+  leagues.forEach(league1 => {
+    league2 = challengeToEdit.league
+    const isSame = JSON.stringify(league1._id) === JSON.stringify(league2._id)
+    //console.log(`league1._id: ${JSON.stringify(league1._id)} league2._id: ${JSON.stringify(league2._id)} : ${isSame}`)
+    if(isSame){
+      league1.selected = true;
+    }
+    });
+  
+  
+  const games = await Game.find().lean(); //.lean() permet de modifier l'objet retourne sans avoir à MaJ le schema
+  games.forEach(game1 => {
+    game2 = challengeToEdit.game
+    const isSame = JSON.stringify(game1._id) === JSON.stringify(game2._id)
+    //console.log(`league1._id: ${JSON.stringify(league1._id)} league2._id: ${JSON.stringify(league2._id)} : ${isSame}`)
+    if(isSame){
+      game1.selected = true;
+    }
+    });
+
   const data = {
-    users,
+    usersContender,
+    usersWinner,
     leagues,
     games,
     challenge: challengeToEdit,
-  }
-  res.render('challenge/edit',data)
+  };
+  console.log('-----------------------------------------data: ', data)
+  res.render('challenge/edit',data);
 });
 
 module.exports = router;
