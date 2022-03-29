76999ceed25f3a5a1f2ee34ff7871eb8aa838802
potapen <po@ta.pen>
2022-03-29 15:11:37 +0200

    before game filtering

--- routes/challenge.routes.js
+++ routes/challenge.routes.js
@@ -140,68 +140,77 @@ router.post("/edit/:challengeID", async (req, res, next) => {
 });
 
 router.get("/edit/:challengeID", async (req, res, next) => {
-  const {challengeID} = req.params
-  console.log('---------------------------challengeID: ', challengeID)
-  const challengeToEdit = await Challenge.findById(challengeID)
-  .populate('contenders')
-  .populate('league')
-  .populate('game')
-  .populate('winners')
-  .populate('stake')
-  .populate('isCompleted')
-  // console.log('---------------------------challengeToEdit: ',challengeToEdit)
-  
-  const usersContender = await User.find(); //pas besoin de .lean()...c'est magigue...
-  usersContender.forEach(person1 => {
-    const isSelected = challengeToEdit.contenders.some(person2 => {
-        const isSame = JSON.stringify(person1._id) === JSON.stringify(person2._id)
-        // console.log(`person1._id: ${JSON.stringify(person1._id)} person2._id: ${JSON.stringify(person2._id)} : ${isSame}`)
-        return isSame
+  try{
+    const {challengeID} = req.params
+    console.log('---------------------------challengeID: ', challengeID)
+    const challengeToEdit = await Challenge.findById(challengeID)
+    .populate('contenders')
+    .populate('league')
+    .populate('game')
+    .populate('winners')
+    .populate('stake')
+    .populate('isCompleted')
+    // console.log('---------------------------challengeToEdit: ',challengeToEdit)
+    
+    const usersContender = await User.find(); //pas besoin de .lean()...c'est magigue...
+    usersContender.forEach(person1 => {
+      const isSelected = challengeToEdit.contenders.some(person2 => {
+          const isSame = JSON.stringify(person1._id) === JSON.stringify(person2._id)
+          // console.log(`person1._id: ${JSON.stringify(person1._id)} person2._id: ${JSON.stringify(person2._id)} : ${isSame}`)
+          return isSame
+      })
+      person1.selected = isSelected
     })
-    person1.selected = isSelected
-  })
-  
-  const usersWinner = await User.find(); //pas besoin de .lean()...c'est magigue...
-  usersWinner.forEach(person1 => {
-    const isSelected = challengeToEdit.winners.some(person2 => {
-        const isSame = JSON.stringify(person1._id) === JSON.stringify(person2._id)
-        // console.log(`person1._id: ${JSON.stringify(person1._id)} person2._id: ${JSON.stringify(person2._id)} : ${isSame}`)
-        return isSame
+    
+    const usersWinner = await User.find(); //pas besoin de .lean()...c'est magigue...
+    usersWinner.forEach(person1 => {
+      const isSelected = challengeToEdit.winners.some(person2 => {
+          const isSame = JSON.stringify(person1._id) === JSON.stringify(person2._id)
+          // console.log(`person1._id: ${JSON.stringify(person1._id)} person2._id: ${JSON.stringify(person2._id)} : ${isSame}`)
+          return isSame
+      })
+      person1.selected = isSelected
     })
-    person1.selected = isSelected
-  })
-
-  const leagues = await League.find(); //pas besoin de .lean()...c'est magigue...
-
-  leagues.forEach(league1 => {
-    league2 = challengeToEdit.league
-    const isSame = JSON.stringify(league1._id) === JSON.stringify(league2._id)
-    //console.log(`league1._id: ${JSON.stringify(league1._id)} league2._id: ${JSON.stringify(league2._id)} : ${isSame}`)
-    if(isSame){
-      league1.selected = true;
-    }
-    });
   
+    const leagues = await League.find({
+      members: req.session.user._id,
+    }); //pas besoin de .lean()...c'est magigue...
   
-  const games = await Game.find().lean(); //.lean() permet de modifier l'objet retourne sans avoir à MaJ le schema
-  games.forEach(game1 => {
-    game2 = challengeToEdit.game
-    const isSame = JSON.stringify(game1._id) === JSON.stringify(game2._id)
-    //console.log(`league1._id: ${JSON.stringify(league1._id)} league2._id: ${JSON.stringify(league2._id)} : ${isSame}`)
-    if(isSame){
-      game1.selected = true;
-    }
-    });
-
-  const data = {
-    usersContender,
-    usersWinner,
-    leagues,
-    games,
-    challenge: challengeToEdit,
-  };
-  console.log('-----------------------------------------data: ', data)
-  res.render('challenge/edit',data);
+    leagues.forEach(league1 => {
+      league2 = challengeToEdit.league
+      const isSame = JSON.stringify(league1._id) === JSON.stringify(league2._id)
+      //console.log(`league1._id: ${JSON.stringify(league1._id)} league2._id: ${JSON.stringify(league2._id)} : ${isSame}`)
+      if(isSame){
+        league1.selected = true;
+      }
+      });
+    
+    
+    const games = await Game.find().lean(); //.lean() permet de modifier l'objet retourne sans avoir à MaJ le schema
+    games.forEach(game1 => {
+      game2 = challengeToEdit.game
+      const isSame = JSON.stringify(game1._id) === JSON.stringify(game2._id)
+      //console.log(`league1._id: ${JSON.stringify(league1._id)} league2._id: ${JSON.stringify(league2._id)} : ${isSame}`)
+      if(isSame){
+        game1.selected = true;
+      }
+      });
+  
+    const data = {
+      usersContender,
+      usersWinner,
+      leagues,
+      games,
+      challenge: challengeToEdit,
+    };
+    console.log('-----------------------------------------data: ', data)
+    res.render('challenge/edit',data);
+  }
+  catch(error){
+    console.log('eeeeeeeeeeeeeeeeeeeeeeeeeeeeeee get /edit/:challengeID');
+    console.error(error);
+    next();
+  }
 });
 
 
