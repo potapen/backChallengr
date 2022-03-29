54658cc37a03a35299ea227de0eb9625ab3868aa
Alexandre Halley <alexandre.m.halley@gmail.com>
2022-03-29 12:11:58 +0200

    new contender model and seed

--- models/Challenge.model.js
+++ models/Challenge.model.js
@@ -2,7 +2,6 @@ const { Schema, model } = require("mongoose");
 
 const challengeSchema = new Schema(
   {
-    contenders: [{ type: Schema.Types.ObjectId, ref: "User", required: true }],
     league: { type: Schema.Types.ObjectId, ref: "League" },
     game: { type: Schema.Types.ObjectId, ref: "Game", required: true },
     winners: [{ type: Schema.Types.ObjectId, ref: "User" }],
