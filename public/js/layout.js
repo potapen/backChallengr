b536d1ac35f6e0c50081d29239c1c692db0700a7
potapen <po@ta.pen>
2022-03-30 17:07:59 +0200

    before axios bis

--- public/js/layout.js
+++ public/js/layout.js
@@ -1,5 +1,7 @@
+// const axios = require('axios')
+
 console.log('this is layout.js')
-window.addEventListener("DOMContentLoaded", (event) => { //otherwise the DOM is not loaded and menuButton returns null
+window.addEventListener("DOMContentLoaded", async (event) => { //otherwise the DOM is not loaded and menuButton returns null
     console.log("DOM entièrement chargé et analysé");
     const menuButton = document.querySelector('#menuButton')
     const navTopElt = document.querySelector('#myTopnav')
@@ -13,5 +15,16 @@ window.addEventListener("DOMContentLoaded", (event) => { //otherwise the DOM is 
           }
     }
     menuButton.addEventListener('click', menuClick)
+
+    async function getOneRegister () {
+        const response = await axios({ //we need to return a promise
+          method: 'GET',
+          url: 'http://localhost:3000/challenge'
+        })
+        console.log('-------------------------------response.data: ', response.data)
+        return response.data
+      }
+
+    // const data = await getOneRegister()
   });
 
