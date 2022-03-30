42547ccbece9e9b20157b9131151be348c51610e
Alexandre Halley <alexandre.m.halley@gmail.com>
2022-03-30 11:42:24 +0200

    create your first league in home

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,10 +1,22 @@
 const isLoggedIn = require("../middleware/isLoggedIn");
+const League = require("../models/League.model");
 
 const router = require("express").Router();
 
 /* GET home page */
-router.get("/", isLoggedIn, (req, res, next) => {
-  res.render("index");
+router.get("/", isLoggedIn, async (req, res, next) => {
+  try {
+    let hasNoLeague = false;
+    const league = await League.findOne({
+      members: req.session.user._id,
+    });
+    if (!league) {
+      hasNoLeague = true;
+    }
+    res.render("index", { hasNoLeague });
+  } catch {
+    next();
+  }
 });
 
 module.exports = router;
