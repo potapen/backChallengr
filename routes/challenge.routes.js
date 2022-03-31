035ee0580b57abbd612fc7d1ad189b3fd0778475
Alexandre Halley <alexandre.m.halley@gmail.com>
2022-03-31 15:38:58 +0200

    improved user interface for challenges

--- routes/challenge.routes.js
+++ routes/challenge.routes.js
@@ -19,16 +19,11 @@ router.get("/", (req, res, next) => {
 router.get("/list", isLoggedIn, async (req, res, next) => {
   try {
     //if there a league in the query, we can show all the challenges of that league
-    console.log(
-      "---------------------------------------- req.query: ",
-      req.query
-    );
     const { league } = req.query;
     if (league) {
       const challenges = await Challenge.find({ league })
-        .populate("league")
-        .populate("game")
-        .populate("contenders");
+        .sort({ createdAt: -1 })
+        .populate("league game contenders winners");
 
       const data = {
         challenges,
@@ -36,8 +31,6 @@ router.get("/list", isLoggedIn, async (req, res, next) => {
       console.log("---------------------------------------- data: ", data);
       res.render("challenge/listWithLeagueID", data);
     } else {
-      //show a page to click on a league first. Once the user selects a league, there is a get with league in the query
-      //ex: http://localhost:3000/challenge/list?league=6242e8b08c623fa3f3698e12
       const leagues = await League.find({
         members: req.session.user._id,
       });
@@ -64,7 +57,9 @@ router.get("/create", isLoggedIn, async (req, res, next) => {
     console.log("req.query: ", req.query);
     const { league } = req.query;
     if (league) {
-      const games = await Game.find({ ownerLeagues: league });
+      const games = await Game.find({
+        $or: [{ ownerLeagues: league }, { isPrivate: false }],
+      });
       const users = await User.find();
       const data = {
         games,
@@ -95,9 +90,8 @@ router.post("/create", async (req, res, next) => {
     console.log("challengeToCreate: ", challengeToCreate);
     const challengeCreated = await Challenge.create(challengeToCreate);
     console.log("challengeCreated: ", challengeCreated);
-    res.render("challenge/done");
+    res.redirect("/");
   } catch (error) {
-    console.log("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeee post /create");
     console.error(error);
     next();
   }
@@ -133,9 +127,8 @@ router.post("/edit/:challengeID", async (req, res, next) => {
       { new: true }
     );
     // res.send(challengeUpdated);
-    res.render("challenge/done");
+    res.redirect("/");
   } catch (error) {
-    console.log("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeee post /edit/:challengeID");
     console.error(error);
     next();
   }
@@ -158,7 +151,6 @@ router.get("/edit/:challengeID", async (req, res, next) => {
       const isSelected = challengeToEdit.contenders.some((person2) => {
         const isSame =
           JSON.stringify(person1._id) === JSON.stringify(person2._id);
-        // console.log(`person1._id: ${JSON.stringify(person1._id)} person2._id: ${JSON.stringify(person2._id)} : ${isSame}`)
         return isSame;
       });
       person1.selected = isSelected;
@@ -169,7 +161,6 @@ router.get("/edit/:challengeID", async (req, res, next) => {
       const isSelected = challengeToEdit.winners.some((person2) => {
         const isSame =
           JSON.stringify(person1._id) === JSON.stringify(person2._id);
-        // console.log(`person1._id: ${JSON.stringify(person1._id)} person2._id: ${JSON.stringify(person2._id)} : ${isSame}`)
         return isSame;
       });
       person1.selected = isSelected;
@@ -183,7 +174,6 @@ router.get("/edit/:challengeID", async (req, res, next) => {
       league2 = challengeToEdit.league;
       const isSame =
         JSON.stringify(league1._id) === JSON.stringify(league2._id);
-      //console.log(`league1._id: ${JSON.stringify(league1._id)} league2._id: ${JSON.stringify(league2._id)} : ${isSame}`)
       if (isSame) {
         league1.selected = true;
       }
@@ -192,21 +182,14 @@ router.get("/edit/:challengeID", async (req, res, next) => {
     const userLeagues = await League.find({
       members: req.session.user._id,
     }).select("_id");
-    console.log("------------------userLeagues: ", userLeagues);
     leagueIdsArray = userLeagues.map((league) => league._id);
-    console.log("------------------leagueIdsArray: ", leagueIdsArray);
 
     const games = await Game.find({
-      $or: [
-        { isPrivate: false },
-        // { ownerLeagues: { $elemMatch: { $in: leagueIdsArray } } },
-        { ownerLeagues: { $in: leagueIdsArray } },
-      ],
+      $or: [{ isPrivate: false }, { ownerLeagues: { $in: leagueIdsArray } }],
     }).lean(); //.lean() permet de modifier l'objet retourne sans avoir à MaJ le schema
     games.forEach((game1) => {
       game2 = challengeToEdit.game;
       const isSame = JSON.stringify(game1._id) === JSON.stringify(game2._id);
-      //console.log(`league1._id: ${JSON.stringify(league1._id)} league2._id: ${JSON.stringify(league2._id)} : ${isSame}`)
       if (isSame) {
         game1.selected = true;
       }
@@ -284,33 +267,12 @@ router.get("/delete/:challengeID", async (req, res, next) => {
 router.post("/delete/:challengeID", async (req, res, next) => {
   try {
     const { challengeID } = req.body;
-    console.log("---------------------------challengeID: ", challengeID);
     const challengeDeleted = await Challenge.findByIdAndDelete(challengeID);
-    console.log(
-      "--------------------------------challengeDeleted:",
-      challengeDeleted
-    );
-    res.render("challenge/done");
+    res.redirect("/");
   } catch (error) {
-    console.log("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeee post /delete/:challengeID");
     console.error(error);
     next();
   }
 });
 
 module.exports = router;
-
-// const challengeSchema = new Schema(
-//     {
-//       contenders: [{ type: Schema.Types.ObjectId, ref: "User", required: true }],
-//       leagues: [{ type: Schema.Types.ObjectId, ref: "League" }],
-//       game: { type: Schema.Types.ObjectId, ref: "Game", required: true },
-//       winners: [{ type: Schema.Types.ObjectId, ref: "User" }],
-//       stake: { type: Number, min: 0, required: true },
-//       isCompleted: { type: Boolean },
-//     },
-//     {
-//       // this second object adds extra properties: `createdAt` and `updatedAt`
-//       timestamps: true,
-//     }
-//   );
