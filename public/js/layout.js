dcec086040a7172d3cc13b9a5b0b7fe43707cdf5
Alexandre Halley <alexandre.m.halley@gmail.com>
2022-03-31 10:35:06 +0200

    revamp navbar

--- public/js/layout.js
+++ public/js/layout.js
@@ -1,30 +1,34 @@
 // const axios = require('axios')
 
-console.log('this is layout.js')
-window.addEventListener("DOMContentLoaded", async (event) => { //otherwise the DOM is not loaded and menuButton returns null
-    console.log("DOM entièrement chargé et analysé");
-    const menuButton = document.querySelector('#menuButton')
-    const navTopElt = document.querySelector('#myTopnav')
-    const menuClick = (event)=>{
-        // console.log('click on menu event ', event)
-        console.log('navTopElt.className :', navTopElt.className)
-        if (navTopElt.className === "topnav") {
-            navTopElt.className += " responsive";
-          } else {
-            navTopElt.className = "topnav";
-          }
+console.log("this is layout.js");
+window.addEventListener("DOMContentLoaded", async (event) => {
+  //otherwise the DOM is not loaded and menuButton returns null
+  console.log("DOM entièrement chargé et analysé");
+  const menuButton = document.querySelector("#menuButton");
+  const navTopElt = document.querySelector("#myTopnav");
+  const menuClick = (event) => {
+    // console.log('click on menu event ', event)
+    console.log("navTopElt.className :", navTopElt.className);
+    if (navTopElt.className === "topnav") {
+      navTopElt.className += " responsive";
+    } else {
+      navTopElt.className = "topnav";
     }
-    menuButton.addEventListener('click', menuClick)
+  };
+  menuButton.addEventListener("click", menuClick);
 
-    async function getOneRegister () {
-        const response = await axios({ //we need to return a promise
-          method: 'GET',
-          url: 'http://localhost:3000/challenge'
-        })
-        console.log('-------------------------------response.data: ', response.data)
-        return response.data
-      }
+  async function getOneRegister() {
+    const response = await axios({
+      //we need to return a promise
+      method: "GET",
+      url: "http://localhost:3000/challenge",
+    });
+    console.log(
+      "-------------------------------response.data: ",
+      response.data
+    );
+    return response.data;
+  }
 
-    // const data = await getOneRegister()
-  });
-
+  // const data = await getOneRegister()
+});
