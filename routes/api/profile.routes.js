4113a83860220ba65ac7f51f7b5f51c6fd0f40a1
Alexandre Halley <alexandre.m.halley@gmail.com>
2022-04-13 15:57:01 +0200

    profile routes

--- routes/api/profile.routes.js
+++ routes/api/profile.routes.js
@@ -1,36 +1,30 @@
 const router = require("express").Router();
 
 const User = require("../../models/User.model");
-const League = require("../../models/League.model");
-const isLoggedIn = require("../../middleware/isLoggedIn");
 const fileUploader = require("../../config/cloudinary.config");
-const app = require("../../app");
-const setProfilePicture = require("../../middleware/setProfilePicture");
+const getUser = require("../../middleware/getUser");
 const cloudinary = require("cloudinary").v2;
 
-router.get("/", isLoggedIn, async (req, res, next) => {
+router.get("/", getUser, async (req, res, next) => {
   try {
-    const user = await User.findById(req.session.user._id);
-    const leagues = await League.find({
-      members: req.session.user._id,
-    });
-    res.render("profile/view-profile", { user, leagues });
+    const user = req.user;
+    res.json({ user });
   } catch (error) {
     console.log(error);
     next();
   }
 });
 
-router.post(
-  "/edit",
+router.put(
+  "/",
   fileUploader.single("coverPicture"),
-  isLoggedIn,
+  getUser,
   async (req, res, next) => {
     try {
-      const id = req.session.user._id;
+      const id = req.user._id;
       let { username } = req.body;
 
-      const newUser = {
+      const updatedUser = {
         username,
       };
       if (req.file) {
@@ -47,17 +41,28 @@ router.post(
             { color: "black", effect: "outline" },
           ],
         });
-        newUser.pictureUrl = newImageUrl;
-        res.app.locals.profilePicture = newImageUrl;
+        updatedUser.pictureUrl = newImageUrl;
       }
-      const user = await User.findByIdAndUpdate(id, newUser, { new: true });
-      req.session.user = user;
-      res.redirect(`/profile`);
+      const updatedUserDoc = await User.findByIdAndUpdate(id, updatedUser, {
+        new: true,
+      });
+
+      res.json({ updatedUserDoc });
     } catch (error) {
       console.log(error);
       next();
     }
   }
 );
+
+router.delete("/", getUser, async (req, res, next) => {
+  try {
+    await User.findByIdAndDelete(req.user._id);
+    res.status(200).send("Successfully deleted the user");
+  } catch (error) {
+    console.log(error);
+    next();
+  }
+});
 
 module.exports = router;
